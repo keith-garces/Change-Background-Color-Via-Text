9a3832979fad3229ffb8fc98e32803cab1ba4f77
kgxx <keithgarces.freelancing@gmail.com>
2023-02-13 00:24:57 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+function changeColor() {
+    var color = document.getElementById("colorInputColor").value;
+    document.body.style.backgroundColor = color;
+    document.getElementById("colorInputText").value = color;
+}
